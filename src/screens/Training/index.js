9d569ee856c31c9c8af2bc2ed1c71f64371d386b
Gerson Campos <87338407+GersonCmps@users.noreply.github.com>
2022-04-04 15:08:47 -0600

    image background course

--- src/screens/Training/index.js
+++ src/screens/Training/index.js
@@ -63,20 +63,18 @@ const Index = (props) => {
   }
 
   const renderList = ({item}) => {
+    console.log(item.coverImage.url)
     return (
       <TouchableOpacity
-        onPress={()=>navigateToCourseDetails(item) }
+        onPress={()=>navigateToCourseDetails(item.item) }
         style={styles.btnCard}
       >
         <View style={styles.container}>
-          {item?.images?.url&&(
-            <ImageBackground
-              resizeMode="cover"
-              source={{uri:item.images.url}}
-              style={styles.backgorundImage}
+            <ImageBackground imageStyle={{ borderRadius: 6}}
+            resizeMode="cover"
+            source={{uri: item?.coverImage?.url}}
+            style={styles.btnCourse}
              />
-          )}
-          <ImageBackground /> 
           <View style={{flex: 1}}>
             <PrimaryText style={styles.cardTitle}>{item.title}</PrimaryText>
           </View>
@@ -99,6 +97,7 @@ const Index = (props) => {
             </View>
           </View>
         </View>
+        <ImageBackground /> 
         <View style={styles.descriptionContainer}>
           { item.summary.length < 50 ? (
               <SecondaryText style={styles.description} color={'#828282'}>{item.summary.slice(0,50)}</SecondaryText>
